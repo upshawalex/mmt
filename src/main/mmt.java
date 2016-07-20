c5307f74aa1cca3cb4c4093365d1749e75d97723
James Alexander Upshaw <alex@AlexDesktopComputer.alexupshaw.com>
2016-07-20 16:35:46 -0500

    Created basic initial files, resolves #1.

---
+++ src/main/mmt.java
@@ -0,0 +1,43 @@
+/*
+ *   mmt.java, the runnable Java application class for this project
+ *   Copyright (C) 2016 Alex Upshaw
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package main;
+
+/**
+ * The application class used to launch mmt.
+ * 
+ * @author Alex Upshaw
+ */
+public class mmt {
+
+	/**
+	 * Run mmt.
+	 * 
+	 * @param args
+	 *            arguments to mmt.
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is unfinished software."
+				+ " Running it as an application is not yet supported.");
+
+		// TODO Implement a runnable main method.
+
+		System.exit(0);
+		return;
+	}
+}
